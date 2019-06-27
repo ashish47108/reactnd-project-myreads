b314a1b1b5d06397beeb8b6b3c52ae96e908f61d
Ashish Jhaveri <ajhaveri@igategegdc.com>
2019-06-27 12:25:17 +0530

    refactor: used array iterator for dropdown categories listing

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -8,29 +8,37 @@ class BookShelfChanger extends Component{
         changeShelf: PropTypes.func.isRequired
     };
 
+    catagories = [        
+        { key: 'move', name: 'Move to...', disabled:true },
+        { key: 'currentlyReading', name: 'Currently Reading' , disabled:false },
+        { key: 'wantToRead', name: 'Want to Read' , disabled:false },
+        { key: 'read', name: 'Read', disabled:false },
+        { key: 'none', name: 'None' , disabled:false }
+      ];
+
     state = {
         currentShelf: this.props.book.shelf        
     };
 
-    changeShelf = (event) => {
+    changeShelf = (event) => {        
         this.props.changeShelf(this.props.book, event.target.value);
         this.setState({
             currentShelf: event.target.value            
-        });
+        });        
     };
 
     render(){
         return(
-            <div className="book-shelf-changer">
+            <div className="book-shelf-changer">                 
                 <select
                     value={this.state.currentShelf}
                     onChange={this.changeShelf}
                 >
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                    {
+                       this.catagories.map((category) => (
+                            <option key={category.key} value={category.key} disabled={category.disabled} >{category.name}</option>
+                        ))
+                    }                    
                 </select>                
             </div>
         );
